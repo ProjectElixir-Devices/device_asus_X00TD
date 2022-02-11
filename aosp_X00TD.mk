4ed81279b91bd02088fd85357bc3be83ad045dd9
Harshhaa Reddy <79190327+harshhaareddy@users.noreply.github.com>
2022-02-11 22:26:47 +0530

    X00TD: Updated FP to latest `February Raven FP`
    Signed-off-by: harshhaareddy <harshhaa03@gmail.com>

--- aosp_X00TD.mk
+++ aosp_X00TD.mk
@@ -37,7 +37,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X00T \
     PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.211205.016.A1 7957957 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220205.003 8069835 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.211205.016.A1/7957957:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys"
