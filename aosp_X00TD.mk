81ec64622c5c78f2588ad606c6ef1776658ba1e2
harshhaareddy <harshhaa03@gmail.com>
2022-09-19 18:12:08 +0530

    X00TD: Update FP to Raven A13 Sept
    Signed-off-by: harshhaareddy <harshhaa03@gmail.com>

--- aosp_X00TD.mk
+++ aosp_X00TD.mk
@@ -44,7 +44,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X00T \
     PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220705.003.A1 8672226 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 13 TP1A.220905.004 8927612 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220705.003.A1/8672226:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:13/TP1A.220905.004/8927612:user/release-keys"
