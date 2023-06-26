e90c5e434c82d9e9e9d79989874c68daf3641f39
H A R S H H A A <112948305+NotHarshhaa@users.noreply.github.com>
2023-06-26 14:02:17 +0530

    X00TD: Updated FP to latest `May Cheetah FP`

--- aosp_X00TD.mk
+++ aosp_X00TD.mk
@@ -43,7 +43,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X00T \
     PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="raven-user 13 TP1A.220905.004 8927612 release-keys"
+    PRIVATE_BUILD_DESC="cheetah-user 13 TQ2A.230505.002 9891397 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "google/raven/raven:13/TP1A.220905.004/8927612:user/release-keys"
+BUILD_FINGERPRINT := "google/cheetah/cheetah:13/TQ2A.230505.002/9891397:user/release-keys"
